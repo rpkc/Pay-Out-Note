42f7d4638c8a245b5a4d246642de1685901b8653
rupak <rupakc770@gmail.com>
2024-12-18 00:08:49 +0530

    Server: db.connect.js working

--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectDB=()=>{
     mongoose.connect('mongodb+srv://rpkc:1234@cluster0.xbfml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=>{
-        console.log("DB Connected");
+        console.log("DB Connected");// if connected properly
     }).catch((err)=>{
-        console.log("DB not Connected "+err.message);
+        console.log("DB not Connected "+err.message);// if not connected
     })
 
 }
